990a502a132b92cc7996ed80e9942b26c0249e0a
EthanChoi <choiyexiang@gmail.com>
2020-02-07 14:00:25 +0800

    completed login UI

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,14 +1,90 @@
 import React from 'react';
+import { makeStyles, withTheme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import BigLogo from '../BigLogo.png'
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { Link } from 'react-router-dom'
 
-
-const LoginPage = () => {
-    return(
-        <>
-        <h1>Login Page</h1>
-        <p>username</p>
-        <p>password</p>
-        </>
-    )
+//style
+const container = {
+  display: 'flex',
+  flexDirection: 'column',
 }
 
-export default LoginPage
+const Timg = {
+  display: 'flex',
+  flexDirection: 'column',
+}
+
+const Tform = {
+  marginTop: '10vh',
+}
+
+const form = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const img = {
+  display: 'block',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  filter: 'invert(100%)',
+}
+
+const btn2 = {
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  width: '200px',
+  backgroundColor: 'blue',
+  color: 'white',
+  borderRadius: '20px',
+  margin: '4px'
+}
+//endstyle
+
+//Don't touch
+const LoginPage = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: 200,
+      textAlign: 'center'
+    },
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function BasicTextFields() {
+  const classes = LoginPage();
+
+  return (
+    <div>
+      <div style={container}>
+        <div style={{ display: 'block' }}>
+          <Link to="/">
+            <IconButton className={classes.margin} size="big ">
+              <ArrowBackIcon fontSize="inherit" />
+            </IconButton>
+          </Link>
+        </div>
+        <div style={Timg}>
+          <img src={BigLogo} alt="logo" width="75%" style={img} />
+        </div>
+        <div style={Tform}>
+          <form className={classes.root} noValidate autoComplete="off" style={form}>
+            <TextField label="Username" />
+            <TextField label="Password" />
+            <button class="ui button" style={btn2}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+  );
+}
